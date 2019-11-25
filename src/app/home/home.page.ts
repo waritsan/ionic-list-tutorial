ec449c7bfe7df651fb6d63c1bb8b87288b7b3ead
Warit Santaputra <waritsan@gmail.com>
2019-11-25 11:18:44 +0700

    Add detail page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export class Character {
+  constructor(
+    public name: string,
+    public description: string,
+    public quote: string,
+    public img: string
+  ) { }
+}
+
+export const characters = [
+  new Character('Finn!!!!', 'I\'m a big deal', 'Listen, I\'ve had a pretty messed up day...', './assets/avatar-finn.png'),
+  new Character('Han', 'Look, kid...', 'I\'ve got enough on my plate as it is, and I...', './assets/avatar-han.png')
+];
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  characters: any;
 
-  constructor() {}
+  constructor() { }
+
+  ngOnInit() {
+    this.characters = characters;
+  }
 
 }
